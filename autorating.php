17ca62ba102fb00e352e82c1d3222da1212cd80e
tlataste <tlataste@macbook-pro-de-tlataste.home>
2018-05-28 17:28:35 +0200

    first commit

---
+++ autorating.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Auto-Rating
+Description: Magic Auto rating 3000
+Version: 0.1
+Author: Lataste Théo
+License: GPL2
+*/
+
+function so_post_40744782( $new_status, $old_status, $post ) {
+    if ( $old_status == 'new' ) {
+        $nbCommentaires = rand(10, 40);
+        $noteMoyenne = rand($nbCommentaires*4, $nbCommentaires*5);
+
+
+        update_post_meta($post->ID, '_kksr_casts', $nbCommentaires);
+        update_post_meta($post->ID, '_kksr_ratings', $noteMoyenne);
+        update_post_meta($post->ID, '_kksr_ips', 'YToxOntpOjA7czo5OiIxMjcuMC4wLjEiO30=');
+        update_post_meta($post->ID, '_kksr_avg', '4');
+    }
+}
+add_action('transition_post_status', 'so_post_40744782', 10, 3 );
